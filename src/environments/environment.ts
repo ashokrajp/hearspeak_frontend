9d2f331bf9a946ee922c34b75f8b6f0868d33a61
ashokr <ashokr@hyperlinkinfosystem.net.in>
2024-07-22 15:16:13 +0530

    second

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 
 
 export const environment = {
-    production: false,
+    production: true,
     API_URL:'https://heart-speak.onrender.com/api/services/',
     // API_URL_AUTH:'http://localhost:9756/api/auth/',
   
