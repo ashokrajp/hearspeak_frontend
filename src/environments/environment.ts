d254b0aea160069ad64430427c379bad1e846eb7
ashokr <ashokr@hyperlinkinfosystem.net.in>
2024-12-18 16:45:38 +0530

    again commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 export const environment = {
     production:true,
     // API_URL:'http://192.168.1.231:9756/api/services/',
-    // API_URL:'http://192.168.1.231:9756/api/services/',
+    // API_URL:'http://192.168.1.231:3055/api/services/',
     API_URL:'https://heart-speak.onrender.com/api/services/',
   
     // API_URL:'http://15.207.221.22:9756/api/services/',
